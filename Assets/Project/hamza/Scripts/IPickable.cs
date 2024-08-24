b77abef808b91c74b4e99d0c9d9764a80e43c91a
Hamza <hamza.mohamed612008@gmail.com>
2024-08-24 17:21:04 +0300

    i mixed the game

---
+++ Assets/Project/hamza/Scripts/IPickable.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+partial class test : MonoBehaviour {
+    private void Start() {
+        IPickable();
+    }
+
+    private void IPickable() {
+        throw new NotImplementedException();
+    }
+
+    public interface Ipickable {
+        bool KeepWorldPosition { get; }
+
+        GameObject PickUp();
+    }
+}
+*/
---
+++ Assets/Project/hamza/Scripts/IUsable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/*
+partial class Tween : MonoBehaviour {
+    private void Start() {
+        IUsable();
+    }
+
+    private void IUsable() {
+        throw new NotImplementedException();
+    }
+}
+    public interface IUsable {
+        void Use(GameObject actor);
+        UnityEvent OnUse { get; }
+    }
+
+
+*/
