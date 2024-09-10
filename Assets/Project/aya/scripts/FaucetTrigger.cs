992f70b69338f3adbd504297eda461801aecf2d5
333aya <122003@sta.edu.eg>
2024-09-10 17:12:52 +0300

    Faucet

---
+++ Assets/Project/aya/scripts/FaucetTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class FaucetTrigger : MonoBehaviour
+{
+    
+    public GameObject water;
+
+   
+    void Start()
+    {
+        water.SetActive(false); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            water.SetActive(true); 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            water.SetActive(false); 
+        }
+    }
+}
