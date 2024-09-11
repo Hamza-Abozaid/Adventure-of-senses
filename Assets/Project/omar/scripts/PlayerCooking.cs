8a553da07019d6f95ba567a64dcbad86458d6052
omarHesham259 <fifarenderz1256@gmail.com>
2024-09-11 15:15:47 +0300

    Making The Burger say

--- Assets/Project/omar/scripts/PlayerCooking.cs
+++ Assets/Project/omar/scripts/PlayerCooking.cs
@@ -1,14 +1,19 @@
+using Fungus;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCooking : MonoBehaviour {
+    public PointManager PointManager;
     [SerializeField] Animator myAnimatorController;
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.name == "Burger") {
+    [SerializeField] Flowchart flowchart;
+    [SerializeField] string blockname;
+    private void OnCollisionEnter(UnityEngine.Collision collision) {
+         if (collision.gameObject.name == "Burger") {
             myAnimatorController.SetBool("Test", true);
-
+        //    PointManager.UpdateScore(1);
+            flowchart.ExecuteBlock(blockname);
         }
     }
-   
+
 }
